139cc1eb769ed885ddc4c3b47ee360e55f491f8e
ELIZA-BETH001 <elizabethnwaugo@gmail.com>
2025-12-28 19:52:20 +0100

    feat: implement Clarity value helpers

---
+++ frontend/src/utils/contract-helpers.js
@@ -0,0 +1,5 @@
+import { uintCV, stringUtf8CV, trueCV, falseCV } from '@stacks/transactions';
+
+export const createUint = (value) => uintCV(value);
+export const createString = (value) => stringUtf8CV(value);
+export const createBool = (value) => value ? trueCV() : falseCV();

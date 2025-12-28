6437483ddf969d4c92c139eeb9da633e227461f6
ELIZA-BETH001 <elizabethnwaugo@gmail.com>
2025-12-28 19:51:50 +0100

    feat: implement chainhooks client skeleton

---
+++ frontend/src/services/chainhooks.js
@@ -0,0 +1,10 @@
+// import { ChainhooksClient } from '@hirosystems/chainhooks-client';
+
+// Placeholder for chainhooks client implementation
+// TODO: Configure API key and endpoints
+
+export const initializeChainhooks = () => {
+    console.log('Initializing Chainhooks Client...');
+    // const client = new ChainhooksClient();
+    // return client;
+};

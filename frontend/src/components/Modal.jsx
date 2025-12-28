e7c260a83e1cdb3c170a09284277337778520af8
ELIZA-BETH001 <elizabethnwaugo@gmail.com>
2025-12-28 19:59:33 +0100

    docs: document Modal component

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,2 +1,5 @@
+/**
+ * Modal Component
+ */
 import PropTypes from 'prop-types';
 export default function Modal({ isOpen, children }) { return isOpen ? <div className="modal">{children}</div> : null; }
